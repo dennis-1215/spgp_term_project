ae2d9756475e1f3abf27797696f6847bf6b93cf5
Kim Jeong Heon <37973668+dennis-1215@users.noreply.github.com>
2024-04-13 15:45:20 +0900

    GameActivity와  GameView 추가

---
+++ app/src/main/java/kr/ac/tukorea/ge/spgp/termproject/GameView.java
@@ -0,0 +1,4 @@
+package kr.ac.tukorea.ge.spgp.termproject;
+
+public class GameView {
+}
--- app/src/main/java/kr/ac/tukorea/ge/spgp/termproject/MainActivity.java
+++ app/src/main/java/kr/ac/tukorea/ge/spgp/termproject/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.tukorea.ge.spgp.termproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void onBtnStartGame(View view) {
+        startActivity(new Intent(this, GameActivity.class));
     }
 }
