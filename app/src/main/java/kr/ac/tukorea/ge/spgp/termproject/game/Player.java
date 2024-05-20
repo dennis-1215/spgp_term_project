ad809484e2d82b61866acf6d1b5b18e06688f50e
Kim Jeong Heon <37973668+dennis-1215@users.noreply.github.com>
2024-05-20 19:16:05 +0900

    발사횟수 증가시 겹쳐서 발사되던 버그 수정

--- app/src/main/java/kr/ac/tukorea/ge/spgp/termproject/game/Player.java
+++ app/src/main/java/kr/ac/tukorea/ge/spgp/termproject/game/Player.java
@@ -27,6 +27,8 @@ public class Player extends AnimSprite {
     public static float BULLET_INTERVAL = 1.5f;
     private static final float offset = 0.75f;
     private float bulletCoolTime = 2.5f;
+    private float multishotCoolTime = 0.f;
+    private int multishot = 0;
     private static int level = 0;
     private static float expMax = 10.0f;
     private static float exp = 0.0f;
@@ -64,22 +66,22 @@ public class Player extends AnimSprite {
     public void update(float elapsedSeconds) {
         super.update(elapsedSeconds);
         bulletCoolTime -= elapsedSeconds;
+        multishotCoolTime += elapsedSeconds;
         if (bulletCoolTime <= 0) {
+            multishot = attackCountLevel;
+            multishotCoolTime = 0.f;
             fire();
-            for(int j = 0; j <= fireNumLevel; ++j) {
-                for (int i = 0; i <= attackCountLevel; ++i) {
-                    if (i % 2 == 1) {
-                        fireBall(10 * ((i + 1) / 2));
-                    } else {
-                        fireBall(-10 * ((i + 1) / 2));
-                    }
-                }
-
-            }
+            shoot();
             bulletCoolTime = BULLET_INTERVAL;
         }
         else if (bulletCoolTime <= BULLET_INTERVAL - BULLET_INTERVAL/10.f){
             idle();
+        }
+
+        if(multishot > 0 && multishotCoolTime >= 0.1f){
+            shoot();
+            multishot--;
+            multishotCoolTime = 0.0f;
         }
     }
 
@@ -95,6 +97,17 @@ public class Player extends AnimSprite {
         float dx = (target[0] - getPosition()[0]);
         float dy = (target[1] - getPosition()[1]);
         targetAngle = Math.toDegrees(Math.acos(dx / Math.sqrt(dx*dx + dy*dy))) * dy / Math.abs(dy);
+    }
+
+    private void shoot(){
+
+        for (int i = 0; i <= fireNumLevel; ++i) {
+            if (i % 2 == 1) {
+                fireBall(10 * ((i + 1) / 2));
+            } else {
+                fireBall(-10 * ((i + 1) / 2));
+            }
+        }
     }
 
     private void fireBall(float angle) {
